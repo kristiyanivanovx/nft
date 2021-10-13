4d9bace451077e28a6a0cac2ff03ab96d401cd2c
Kristiyan Ivanov <kristianiva88@gmail.com>
2021-10-13 21:10:24 +0300

    feat: implement card

--- pages/demo/index.jsx
+++ pages/demo/index.jsx
@@ -1,12 +1,22 @@
 import Header from "../../src/components/header/Header";
 import * as React from "react";
 import Avatar from '../../src/components/avatar/Avatar';
+import MyCard from '../../src/components/card/Card';
 
 export default function Index() {
+    let avatar = <Avatar url={'/images/avatar.png'} />;
+
     return (
-       <>
+       <div>
            <Header />
-           <Avatar url={'/images/avatar.png'} />
-       </>
+           <MyCard
+               name={'Clock'}
+               likes={'1.1K'}
+               mediaUrl={'/images/nft.jpg'}
+               user={avatar}
+               price={'13'}
+               currency={'ETH'}
+           />
+       </div>
     )
 }
