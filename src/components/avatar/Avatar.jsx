bad6e909e33c2f1ec40a2fa91fe6d8773e909587
Kristiyan Ivanov <kristianiva88@gmail.com>
2021-10-13 19:48:22 +0300

    fix problems

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,11 +1,11 @@
 import * as React from "react";
 import styles from './Avatar.module.scss';
 
-export default function Avatar({url, verified = false }) {
+export default function Avatar({url, size = 90, verified = false }) {
     return (
-        <div className={'avatar'} width={123} height={123}>
+        <div className={'avatar'} width={size} height={size}>
             <img src={url} />
-            <div className={'badge'}></div>
+            { verified ? ( <div className={'badge'}><img src={'/images/verified.svg'} /></div> ) : null }
         </div>
     )
 }
