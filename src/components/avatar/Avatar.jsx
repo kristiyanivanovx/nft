194858d0e5d70e60ac51fdcc20d7bd2a544a468f
Kristiyan Ivanov <kristianiva88@gmail.com>
2021-10-13 20:09:25 +0300

    fix

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -4,10 +4,10 @@ import styles from './Avatar.module.scss';
 export default function Avatar({url, size = 90, verified = false }) {
     return (
         <div className={'avatar'} width={size} height={size}>
-            <img  width={size} src={url} />
+            <img className={'image'} width={size} src={url} />
             { verified ? (
                 <div className={'badge'}>
-                    <img className={'image'} src={'/images/verified.svg'} />
+                    <img  src={'/images/verified.svg'} />
                 </div> ) : null
             }
         </div>
