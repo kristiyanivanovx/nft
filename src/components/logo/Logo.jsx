c48b0d87eb8c1f7bc293c0b2846210430ff863f3
Kristiyan Ivanov <kristianiva88@gmail.com>
2021-10-13 17:15:42 +0300

    simplify

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,25 +1,7 @@
 import * as React from "react";
-import Container from "@mui/material/Container";
-import Button from "@mui/material/Button";
-import Grid from "@mui/material/Grid";
-import Link from "../link/Link";
-import Image from 'next/image';
 
 export default function Logo({ type = 'default' }) {
     return (
-        (
-            type === 'default' ? (
-            <div>
-                <img
-                    src={'/images/logo.svg'}>
-                </img>
-            </div>
-        ) : (
-           type === 'muted' ? (
-               <img
-                   src={'/images/logo-muted.svg'}>
-               </img>
-           ) : null
-        )
-    ));
+        <img src={type === 'default' ? '/images/logo.svg': '/images/logo-muted.svg' } />
+    )
 }
