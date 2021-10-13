8d2fe7b47fd4759b82c3afa6f86a725ea416c65e
Kristiyan Ivanov <kristianiva88@gmail.com>
2021-10-13 23:01:34 +0300

    idk

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -3,17 +3,20 @@ import Avatar from "../avatar/Avatar";
 import styles from './User.module.scss';
 
 export default function User({
-     name = '',
-     info= '',
-     avatar = '',
-     // size = 55,
-     // verified = false
+    name = '',
+    info= '',
+    avatar = '',
+
+    size ,
+    // size = 55,
+    verified
+    // verified = false
 }) {
     return (
         <div className={styles.user}>
             <div className={styles.name}>{name}</div>
             <div className={styles.info}>{info}</div>
-            <Avatar url={avatar} />
+            <Avatar url={avatar} size={size} verified={verified} />
         </div>
     )
 }
