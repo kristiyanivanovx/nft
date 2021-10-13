6a5c8da29526e2686b66278f19e86f53400411e9
Kristiyan Ivanov <kristianiva88@gmail.com>
2021-10-13 22:35:13 +0300

    quickfix

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,16 +1,12 @@
 import * as React from "react";
 import styles from './Avatar.module.scss';
 
-export default function Avatar({ url, size = 90, verified = false }) {
+// export default function Avatar({ url, size = 90, verified = false }) {
+export default function Avatar({ url, size, verified }) {
     return (
         <div className={'avatar'} width={size} height={size}>
             <img className={'image'} width={size} height={size} src={url} />
-            { verified ? (
-                // <div className={'badge'}>
-                <img className={'badge'} src={'/images/verified.svg'} />
-                // </div>
-                ) : null
-            }
+            { verified ? (<img className={'badge'} src={'/images/verified.svg'} />) : null }
         </div>
     )
 }
